be07bbb8946efb4016df5d6c9d04dbbded2681fe
Tomasz Kopacz <tkopacz@tomaszkopacz.com>
2015-09-08 11:31:52 +0200

    Update for RTM

--- RPI2_SPI_MCP3002_Potencjometr/RPI2_SPI_MCP3002_Potencjometr/MainPage.xaml.cs
+++ RPI2_SPI_MCP3002_Potencjometr/RPI2_SPI_MCP3002_Potencjometr/MainPage.xaml.cs
@@ -19,11 +19,13 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace RPI2_SPI_MCP3002_Potencjometr {
+namespace RPI2_SPI_MCP3002_Potencjometr
+{
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page {
+    public sealed partial class MainPage : Page
+    {
         //Podłączenia MCP3002 
         /*
         VSS - do GND
